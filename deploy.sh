8f854dfbb8a09693054662aa0f383699b1ea3264
nhnajibullah <najib@ce.undip.ac.id>
2018-01-01 00:23:44 +0700

    edit

--- deploy.sh
+++ deploy.sh
@@ -22,7 +22,7 @@ else
 		if [ "$f" != ".travis.yml" ] && [ "$f" != "deploy.sh" ] && [ "$f" != "test.js" ] && [ "$f" != "package.json" ]
 		then
 	 		echo "Uploading $f"
-	 		curl --ftp-create-dirs -T $f -u $FTP_USER:$FTP_PASS ftp://$FTP_HOST/$f
+	 		curl --ftp-create-dirs -T $f -u $FTP_USERNAME:$FTP_PASSWORD ftp://$FTP_HOST/$f
 		fi
 	done
 fi
